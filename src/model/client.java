215dd3cf46b7feda75de02022fca9864b4e74b20
xStaticVoid <dthurston1996@gmail.com>
2018-01-03 19:18:23 -0600

    first commit

---
+++ src/model/client.java
@@ -0,0 +1,58 @@
+package model;
+
+/**
+ * Client class to handle client things.
+ *
+ * @author Dustin Thurston (@xStaticVoid)
+ */
+public class client {
+
+    /** Ip address*/
+    private String ip;
+
+    /** Port number*/
+    private String port;
+
+    /** Username*/
+    private String username;
+
+    /** admin rights? */
+    private boolean admin;
+
+    /**
+     * Default Constructor.  Each provided from intro screen.
+     * @param ip address to connect to
+     * @param port number the server is on
+     * @param username the user wants to be recognized as
+     */
+    public client(String ip, String port, String username){
+        this.ip = ip;
+        this.port = port;
+        this.username = username;
+        this.admin = false; //by default.  Server will decide if admin or not.
+    }
+
+    /**
+     * Set's new username
+     * @param newName new name to try.
+     */
+    public void setUsername(String newName){
+        this.username = newName;
+    }
+
+    /**
+     * Set's user's admin rights.
+     * @param isAdmin true or false.
+     */
+    public void setAdmin(boolean isAdmin){
+        this.admin = isAdmin;
+    }
+
+    /**
+     * Get the username
+     * @return the username
+     */
+    public String getUsername(){
+        return this.username;
+    }
+}
